dc6dca1370155b4682f3eb8a06c25f565bee6047
Hazar <92859817+HazarBakir@users.noreply.github.com>
2025-10-18 16:09:45 +0300

    Update React version and add react-router-dom
    Downgraded React and ReactDOM from v19 to v18 and added react-router-dom as a dependency. Cleaned up App.css by removing unused styles, and updated App.jsx with additional motivational messages. Minor formatting changes applied to main.jsx and index.html.

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ function App() {
     <>
       <div className="App">
         <h1>Hello World</h1>
+        <h3>I know you suck at start</h3>
+        <h3>But <h1>DON'T</h1> you fucking <h1>GIVE UP!</h1></h3>
       </div>
     </>
   );
