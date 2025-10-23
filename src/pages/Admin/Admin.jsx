f21fd26fba045f4d0251c077611cb1993f46e9c0
Hazar <92859817+HazarBakir@users.noreply.github.com>
2025-10-23 13:26:22 +0300

    refactor: remove unused useEffect and integrate GetUser component
    Eliminated the useEffect hook for fetching the user email and replaced it with a GetUser component to streamline the email retrieval process in the Admin page.

--- src/pages/Admin/Admin.jsx
+++ src/pages/Admin/Admin.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { supabase } from "../../supabaseClient";
 import { AdminEmailForm } from "../../components/AdminEmailForm";
 import { useNavigate } from "react-router-dom";
@@ -19,31 +19,14 @@ export function Admin() {
       console.error("Error updating email:", error.message);
       return;
     }
-
     if (data) {
       console.warn("Email update successful:", data);
     }
   }
 
-  useEffect(() => {
-    async function fetchUser() {
-      const {
-        data: { user },
-        error,
-      } = await supabase.auth.getUser();
-      if (!user) {
-        console.error("No user logged in:", error);
-        return;
-      }
-
-      setEmail(user.email);
-    }
-
-    fetchUser();
-  }, []);
-
   return (
     <>
+      <GetUser setEmail={setEmail} />
       <div className="admin-container">
         <h2>Welcome {email} </h2>
         <div className="admin-credential-container">
