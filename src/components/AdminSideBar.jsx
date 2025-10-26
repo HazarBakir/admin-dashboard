fe3fc0a59b135999bb74dba1f35035c447756960
Hazar <92859817+HazarBakir@users.noreply.github.com>
2025-10-26 13:42:04 +0300

    feat: add AdminSidebar component for navigation in Admin interface
    Introduced the AdminSidebar component, providing a structured menu for navigation within the Admin interface. This addition enhances user experience by allowing quick access to key sections such as Dashboard, User Settings, and Settings.

---
+++ src/components/AdminSideBar.jsx
@@ -0,0 +1,18 @@
+export function AdminSidebar() {
+  return (
+    <aside className="admin-sidebar">
+      <h3>Menu</h3>
+      <ul>
+        <li>
+          <a href="">Dashboard</a>
+        </li>
+        <li>
+          <a href="">User Settings</a>
+        </li>
+        <li>
+          <a href="">Settings</a>
+        </li>
+      </ul>
+    </aside>
+  );
+}
