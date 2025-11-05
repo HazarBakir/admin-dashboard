46db867f5a49974bfa5e75a8e9c924a0f58663af
HazarBakir <hazar_bakir@protonmail.com>
2025-11-05 23:40:57 +0300

    feat: add AdminSidebar component for navigation in admin panel
    Introduced the AdminSidebar component, providing a structured navigation menu for administrators, enhancing usability and organization within the admin panel.

--- src/components/Admin/AdminSideBar.jsx
+++ src/components/Admin/AdminSideBar.jsx
@@ -2,10 +2,10 @@ export function AdminSidebar() {
   return (
     <aside className="admin-sidebar">
       <h3>Admin Panel</h3>
-      <hr style={{ border: '1px solid #1f2937', margin: '0rem 0' }} />
+      <hr/>
       <ul>
         <li>
-          <a href="">Dashboard-mock</a>
+          <a href="">Configuration</a>
         </li>
         <li>
           <a href="">User Settings-mock</a>
